fdeafe009dfeaaec443c30bcda86aad9e43560cd
fatinzulkhair <90077234+fatinzulkhair@users.noreply.github.com>
2023-07-01 05:07:55 +0700

    Update word_to_excel.py

--- word_to_excel.py
+++ word_to_excel.py
@@ -13,7 +13,7 @@ st.markdown(
 
 st.markdown(
     """
-<nav class="navbar fixed-top navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">**Fatin Zulkhair**</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
