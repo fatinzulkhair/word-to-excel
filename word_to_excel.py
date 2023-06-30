a624baf032102b56b3ffc39e049a0aa0938b1f12
fatinzulkhair <90077234+fatinzulkhair@users.noreply.github.com>
2023-07-01 04:56:47 +0700

    Update word_to_excel.py

--- word_to_excel.py
+++ word_to_excel.py
@@ -4,29 +4,42 @@ import pandas as pd
 from io import BytesIO
 import re
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+st.set_page_config(layout="wide")  # Ukuran layar
 
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+st.markdown(
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">',
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
+<nav class="navbar fixed-top navbar-light bg-light">
+  <a class="navbar-brand" href="#">**Fatin Zulkhair**</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
-      </li>
+        <a class="nav-link" href="#">Link</a>
     </ul>
   </div>
 </nav>
-""", unsafe_allow_html=True)
+""",
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """# **Word to Excel App**
+A simple app to convert *multiple choice answers* into *table* .
+"""
+)
+
+st.header("**Select File**")
 
 def word_to_excel(nama_file):
     doc = Document(nama_file)
@@ -104,3 +117,16 @@ if File_word is not None:
     table = word_to_excel(File_word).rename_axis("No.").reset_index()
     name_before_extension = re.match("(^.+)(\.\D+)", File_word.name).group(1)
     download_button(table, file_name=f"{name_before_extension}.xlsx")
+
+st.info(
+    "Credit: Created by [FatinZulkhair](https://id.linkedin.com/in/m-fatin-zulkhair-yusuf-855427211)"
+)
+
+st.markdown(
+    """
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+""",
+    unsafe_allow_html=True,
+)
